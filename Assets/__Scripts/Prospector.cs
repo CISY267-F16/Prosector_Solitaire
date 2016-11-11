ad0a12124a94cd8219ac63a15c126522609e9b5c
dthoma6403 <dthoma6403@gmail.com>
2016-11-11 13:58:22 -0800

    Parsing XML Files

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -8,6 +8,9 @@ public class Prospector : MonoBehaviour {
 	public Deck	deck;
 	public TextAsset  deckXML;
 
+	public Layout layout;
+	public TextAsset layoutXML;
+
 	void Awake() {
 		S = this; // Set up a Singleton for Prospector
 	}
@@ -15,5 +18,11 @@ public class Prospector : MonoBehaviour {
 	void Start()  {
 		deck = GetComponent<Deck>();  // Get the deck
 		deck.InitDeck (deckXML.text);  // Pass deckXML to it
+		Deck.Shuffle (ref deck.cards); // This shiffles the deck
+		// the ref keyword passes a reference to deck.cards, which allows deck.suffle to modify it
+
+		layout = GetComponent<Layout> ();  //get the layout
+		layout.ReadLayout (layoutXML.text);  // Passs LayoutXML to it
+
 	}
 }
